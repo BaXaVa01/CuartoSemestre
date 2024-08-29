bff1cf5285956f0ee5342eac5991320a7494c9ea
BaXaVa <barkalovalexander01@gmail.com>
2024-08-29 00:41:01 -0600

    ultimo trabajo de algebra finalizado

--- Algebra Lineal/programas/main.py
+++ Algebra Lineal/programas/main.py
@@ -7,68 +7,28 @@ class Operacion(Enum):
     restar = False
     sumar = True
 
-
-
 def main():
     
-    matrix=[[ 4 ,3 ,4 ,2 ],
-            [ 2 ,4 ,1 ,2 ],
-            [ 4 ,2 ,5 ,7 ]]
+    matrix=[[ 4 ,0 ,4 ,2,3 ],
+            [ 0 ,1 ,1 ,2,3 ],
+            [ 0 ,4,0 ,7 ,6],
+            [3,6,2,5,7]]
     printMatrix(matrix)
-    pivoteoMax(matrix)
+    try:
+        pivoteoMax(matrix)
+
+        print("##############################")
+        print("resultados:")
+
+        printMatrix(matrix)
+
+        for x in range(len(matrix)):
+            print(f"x{x+1} = {matrix[x][3]}")
+    except:
+        print("La matriz es indefinida")
 
 
-
-
-
-def pivoteo(matrix):
-
-    printMatrix(matrix)
-    rowObjective = 0
-
-    for fila in matrix:
-        if fila[0] == 1: 
-            rowObjective = fila[0]
-            break
-
-    if rowObjective != 0:
-        alternarfilas(matrix, 0, rowObjective)
-
-        for row in matrix:
-            row = OperateRows(row, matrix[0], False)
-
-    else:
-        pass\
-        
-        #         printMatrix(matrix)
-        # rowObjective = 0
-
-        # for fila in matrix:
-        #     if fila[0] == 1: 
-        #         rowObjective = fila[0]
-        #         break
-
-        # if rowObjective != 0:
-        #     alternarfilas(matrix, 0, rowObjective)
-        #     printMatrix(matrix)
-
-        #     for index in range(len(matrix)):
-        #         if index == 0: continue
-                
-        #         operacion = Operacion.restar
-        #         if matrix[index][0] < 0: operacion = Operacion.sumar
-                
-        #         matrix[index] = OperateRows(matrix[index], multiplyRow(matrix[0][:], matrix[index][0]), operacion.value )
-
-        #         printMatrix(matrix)
-        # else:
-        # matrix[0] = multiplyRow(matrix[0], 1/(matrix[0][0]))     
-        # printMatrix(matrix)
-
-        
-        # # pivoteo(matrix)
-    
-    printMatrix(matrix, True)
+      
 
 def pivoteoMax(matrix):
     row:int = 0
@@ -85,19 +45,24 @@ def pivoteoMax(matrix):
         for fila in range(row + 1, len(matrix)):
             
             if matrix[fila][column] == 0: continue
-
             
-
-
             operation = False
             if matrix[fila][column] < 0: operation = True
             
             matrix[fila] = OperateRows(matrix[fila], multiplyRow(matrix[row][:],abs(matrix[fila][column])), operation ) 
             printMatrix(matrix)
+
+        for filaArriba in range(row, 0, -1):
+            # if row == 0: break
             
-        
+            operation = False
+            if matrix[filaArriba - 1][column] < 0: operation = True
+
+            matrix[filaArriba -1] = OperateRows(matrix[filaArriba -1],multiplyRow(matrix[row][:], abs(matrix[filaArriba -1][column])), operation)
+            
+            printMatrix(matrix)
+            
+
         row += 1
 
-def pivote(matrix, ):
-    pass
 main()
